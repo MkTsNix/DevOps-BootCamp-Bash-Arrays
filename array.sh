e4e748f9427bacbbc1035cf6a9de2d7be9f402dd
Mikhail Tsay <37892513+MkTsNix@users.noreply.github.com>
2023-01-07 23:51:17 +0600

    Update array.sh

--- array.sh
+++ array.sh
@@ -5,8 +5,8 @@
 count=1
 for i in {a..z}; do
     [[ $count -gt $1 ]] && break
-    mkdir folder_$i
+    mkdir folder_"$i"
     ((count++))
 done
 
-echo $1 "folders created: $(ls -d folder* | grep -v / | xargs echo | sed 's/ /, /g')"
+echo "$1" "folders created: $(ls -d folder* | grep -v / | xargs echo | sed 's/ /, /g')"
