4e083085795b16cecd8a42e8f05633e6bd6bd78c
Mikhail Tsay <37892513+MkTsNix@users.noreply.github.com>
2023-01-10 20:15:16 +0600

    Update array.sh

--- array.sh
+++ array.sh
@@ -2,15 +2,32 @@
 
 # Place your code here
 
-count=1
-phrase="folders"
-for i in {a..z}; do
-    [[ $count -gt $1 ]] && break
-    mkdir folder_"$i"
-    ((count++))
+#count=1
+#phrase="folders"
+#for i in {a..z}; do
+#    [[ $count -gt $1 ]] && break
+#    mkdir folder_"$i"
+#    ((count++))
+#done
+
+#if [[ $1 -eq 1 ]]; then phrase="folder"
+#fi
+
+#echo "$phrase created: $(ls -d folder* | grep -v / | xargs echo | sed 's/ /, /g')"
+
+chars=( {a..z} )
+for ((i=0; i<$1; i++))
+do
+mkdir "folder_${chars[i]}"
 done
 
-if [[ $1 -eq 1 ]]; then phrase="folder"
+if [[ $1 == 1 ]]
+then
+echo -n "$1 folder created: "
+else
+echo -n "$1 folders created: "
 fi
 
-echo "$phrase created: $(ls -d folder* | grep -v / | xargs echo | sed 's/ /, /g')"
+#ls -d folder_* | sed "s/ /, /" #| tr ": " ", " | sed 's/. $//'
+
+echo -n $(ls -d) folder_* | sed "s/. //" | sed "s/ /, /g"
