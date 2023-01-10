aba2d88e8127d249ba81a9f2b3ee6e9419abcfbf
Mikhail Tsay <37892513+MkTsNix@users.noreply.github.com>
2023-01-10 20:38:48 +0600

    Update array.sh

--- array.sh
+++ array.sh
@@ -1,3 +1,19 @@
 #!/bin/bash
 
 # Place your code here
+
+
+chars=( {a..z} )
+for ((i=0; i<$1; i++))
+do
+mkdir "folder_${chars[i]}"
+done
+
+if [[ $1 == 1 ]]
+then
+ output="$1 folder created: "
+else
+ output="$1 folders created: "
+fi
+
+echo "$output: $(ls -d folder* | grep -v / | xargs echo | sed 's/ /, /g')"
