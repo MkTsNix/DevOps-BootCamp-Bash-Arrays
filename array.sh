ec1d78f05c551a85dcdc14c0e14a3e389e8a7f4b
Mikhail Tsay <37892513+MkTsNix@users.noreply.github.com>
2023-01-10 15:13:29 +0600

    Update array.sh

--- array.sh
+++ array.sh
@@ -3,10 +3,14 @@
 # Place your code here
 
 count=1
+phrase="folders"
 for i in {a..z}; do
     [[ $count -gt $1 ]] && break
     mkdir folder_"$i"
     ((count++))
 done
 
-echo "$1" "folders created: $(ls -d folder* | grep -v / | xargs echo | sed 's/ /, /g')"
+if [[ $1 -eq 1 ]]; then phrase="folder"
+fi
+
+echo "$phrase created: $(ls -d folder* | grep -v / | xargs echo | sed 's/ /, /g')"
